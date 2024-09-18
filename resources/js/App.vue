4e8c8701286de5efe03d30aa0d71698804f4b7a1
laravelblog <bilgeyilmaz121@gmail.com>
2024-09-18 17:46:55 +0300

    initial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,78 @@
+<template>
+    <div id="wrapper">
+        <!-- sidebar -->
+        <div class="sidebar" :class="{showOverlay:overlayVisibility}">
+            <span class="closeButton" @click="hideOverlay">&times;</span>
+            <p class="brand-title"><a href="">Wisdom Blog</a></p>
+
+            <div class="side-links">
+                <ul>
+                    <li>><router-link @click="hideOverlay" :to="{name:'Home'}">Home</router-link></li>
+                    <li><router-link @click="hideOverlay" :to="{name:'Blog'}">Blog</router-link></li>
+                    <li><router-link @click="hideOverlay" :to="{name:'About'}">About</router-link></li>
+                    <li><router-link @click="hideOverlay" :to="{name:'Contact'}">Contact</router-link></li>
+                    <li><router-link @click="hideOverlay" :to="{name: 'Login'}">Login</router-link></li>
+                    <li><router-link @click="hideOverlay" :to="{name: 'Register'}">Register</router-link></li>
+                    <li><router-link @click="hideOverlay" :to="{name: 'Dashboard'}">Dashboard</router-link></li>
+                </ul>
+            </div>
+
+            <!-- sidebar footer -->
+            <footer class="sidebar-footer">
+                <div>
+                    <a href=""><i class="fab fa-facebook-f"></i></a>
+                    <a href=""><i class="fab fa-instagram"></i></a>
+                    <a href=""><i class="fab fa-twitter"></i></a>
+                </div>
+
+                <small>&copy Wisdom Blog</small>
+            </footer>
+        </div>
+        <!-- Menu Button -->
+        <div class="menuButton" @click="showOverlay">
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+        <!-- main -->
+        <main class="container">
+            <router-view></router-view>
+
+        </main>
+
+        <!-- Main footer -->
+        <footer class="main-footer">
+            <div>
+                <a href=""><i class="fab fa-facebook-f"></i></a>
+                <a href=""><i class="fab fa-instagram"></i></a>
+                <a href=""><i class="fab fa-twitter"></i></a>
+            </div>
+            <small>&copy Wisdom Blog</small>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            overlayVisibility:false,
+        };
+    },
+    methods: {
+        showOverlay() {
+            this.overlayVisibility = true;
+        },
+        hideOverlay() {
+            this.overlayVisibility = false;
+        },
+    },
+};
+</script>
+<style scoped>
+.showOverlay {
+    width: 100%;
+    z-index: 5;
+}
+</style>
+<script setup>
+</script>
---
+++ resources/js/pages/About.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="single-blog-post">
+        <h1>About Me</h1>
+        <div class="single-blog-post-ContentImage" data-aos="fade-left">
+            <img src="images/photography.jpg" alt="" />
+        </div>
+
+        <div>
+            <p class="about-text">
+                Ross, Faydacılık ve Kantçı Etik gibi tekçi ahlak kuramlarının ahlakı aşırı derece basite indirgediklerini söylemiş
+                ve sezgilere ters düştüklerini iddia etmiştir. Faydacılar, her zaman için toplam mutluluğu maksimize etmemiz gerektiğini
+                söyleyip dururlar. Ancak mutluluğu maksimize etmek bazen masumlara işkence etmek veya onları öldürmek gibi ahlaken epey yanlış
+                görünen eylemleri mazur görmeyi gerektirebilir. Kantçılar ise, iyi amaçlara ulaşmak için her zaman insanlara saygılı ve dürüst
+                davranmamız, başkalarına asla ama asla zarar vermememiz gerektiğini iddia eder. Fakat, hiç değilse bile en azından bir kişiye
+                saygısızlık gibi görünen bir şeyi yapmaktan kendimizi alamayacağımız durumlar içinde olabiliriz; örneğin, daha az saygınlığın
+                veya zararlı bir şeyin ortaya çıkmasını engellemek adına belki de yalan söylememiz veya birine zarar vermemiz gerekebilir.
+                <br /><br />
+
+            </p>
+        </div>
+    </section>
+</template>
